f5a0fbce0989ebd30197ccce04683a735468e203
Yixiao Li <liyixiao7@gmail.com>
2017-04-12 02:37:54 +0900

    support gcc 6.3.1 (gcc-arm-none-eabi-6-2017-q1-update)

--- common/ev3api/src/ev3api.c
+++ common/ev3api/src/ev3api.c
@@ -52,3 +52,11 @@ void _app_init_task(intptr_t unused) {
  * This workaround can suppress compiler errors.
  */
 void *__dso_handle __attribute__((weak))=0;
+
+/**
+ * TODO:
+ * This is a dummy function to workaround a potential bug in 'gcc-arm-none-eabi-6-2017-q1-update',
+ * since the global destructors are not supported currently.
+ * See 'http://wiki.osdev.org/Calling_Global_Constructors' for details of '.fini_array' and '_fini' in ARM.
+ */
+void __attribute__((weak)) _fini() {}
