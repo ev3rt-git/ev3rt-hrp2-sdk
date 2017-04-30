8e9534dab6f865368c40c4374b7029b24824d108
Jarek Páral <paral.jarek@gmail.com>
2017-04-30 19:38:39 +0200

    Create test project for ev3cxx API

---
+++ workspace/ev3cxx_test/app.cpp
@@ -0,0 +1,50 @@
+/**
+ * This sample program for CPP EV3RT API.
+ */
+
+#include "ev3api.h"
+#include "app.h"
+
+#include "libcpp-test.h"
+
+#define DEBUG
+
+#ifdef DEBUG
+#define _debug(x) (x)
+#else
+#define _debug(x)
+#endif
+
+class TestClass {
+public:
+    TestClass() {
+        //static char buf[256];
+        //sprintf(buf, "Object has been created.");
+        //ev3_lcd_draw_string(buf, 0, 16);
+        member = 0x12345678;
+    }
+
+    void testMethod() {
+        static char buf[256];
+        sprintf(buf, "Member is 0x%08x.", member);
+        ev3_lcd_draw_string(buf, 0, 32);
+    }
+private:
+    int member;
+};
+
+auto obj2 = new TestClass();
+
+void main_task(intptr_t unused) {
+    // Test global constructor
+    obj2->testMethod();
+
+    // Test function in static library
+    libcpp_test_c_echo_function(777);
+
+    // Test class in static library
+    LibSampleClass a;
+    a.draw();
+
+    //auto obj = new TestClass();
+}
