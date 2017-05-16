8229ee4c290229d43e81865004bcea044fef2244
Jarek Páral <paral.jarek@gmail.com>
2017-05-16 22:14:26 +0200

    Change structure of folders with ev3cxx_tests

--- workspace/ev3cxx_tests/ev3cxx-test/app.cpp
+++ workspace/ev3cxx_tests/ev3cxx-test/app.cpp
@@ -2,9 +2,13 @@
  * This sample program for CPP EV3RT API.
  */
 
+#include <cstdlib>
+
 #include "ev3api.h"
 #include "ev3cxx.h"
 #include "app.h"
+
+using ev3cxx::display;
 
 void main_task(intptr_t unused) {
     // // EV3API lib - C solution
@@ -17,7 +21,7 @@ void main_task(intptr_t unused) {
     // int32_t LCD_FONT_HEIGHT, LCD_FONT_WIDTH;
     // ev3_font_get_size(EV3_FONT_MEDIUM, &LCD_FONT_WIDTH, &LCD_FONT_HEIGHT);
 
-    // FILE *bt = ev3_serial_open_file(EV3_SERIAL_BT);   
+    // bt *bt = ev3_serial_open_bt(EV3_SERIAL_BT);   
     // ev3_lcd_draw_string(lcdText, 0, LCD_FONT_HEIGHT * 0);
     // sprintf(lcdText, "isOpen(): %i", (bt == NULL) ? 'Y' : 'N');
     // ev3_lcd_draw_string(lcdText, 0, LCD_FONT_HEIGHT * 1);
@@ -28,79 +32,79 @@ void main_task(intptr_t unused) {
     // ev3_lcd_draw_string("fputc('a', bt)", 0, LCD_FONT_HEIGHT * 3);
     
     // ev3cxx lib - C++ solution
-    //File file("test_file.txt");
-    ev3cxx::Bluetooth file{}; // same workflow as with file
-    file.open();
+    //bt bt("test_bt.txt");
+    ev3cxx::Bluetooth bt{}; // same workflow as with bt
+    bt.open();
 
     display.setTextLine(4);
-    display.format("Test File\n");    
-    display.format("isOpen(): % \n") % (file.isOpen() ? 'Y' : 'N');
-    display.format("isConnected(): % \n") % (file.isConnected() ? 'Y' : 'N'));
+    display.format("Test bt\n");    
+    display.format("isOpen(): % \n") % (bt.isOpen() ? 'Y' : 'N');
+    display.format("isConnected(): % \n") % (bt.isConnected() ? 'Y' : 'N');
 
-    if(!file.isOpen())
+    if(!bt.isOpen())
         std::exit(1); // if is not bluetooth open => exit() => end program
 
     display.format("write('a')\n");
-    file.write('a');
+    bt.write('a');
 
-    display.format("format(file,...)\n");
+    display.format("format(bt,...)\n");
 
-    format(file, "Test File\n");
-    format(file, "\ntest write to file \n\tdata: %  % \n") % 125 % 98.76;
+    format(bt, "Test bt\n");
+    format(bt, "\ntest write to bt \n\tdata: %  % \n") % 125 % 98.76;
 
-    file.close();
-    display.format("close(): % \n") % file.isOpen();
+    bt.close();
+    display.format("close(): % \n") % bt.isOpen();
 
     tslp_tsk(2000);
     display.resetScreen();
 
 
-    // Open new file
-    file.open("new_file.txt");
-    display.format("%  - new_file\n") % "File";
-    display.format("isOpen(): % \n") % file.isOpen();
+    // Open new bt
+    bt.open("new_bt.txt");
+    display.format("%  - new_bt\n") % "bt";
+    display.format("isOpen(): % \n") % bt.isOpen();
     
-    if(!file.isOpen())
+    if(!bt.isOpen())
         std::exit(1); // if is not open, exit()
     
-    //format(file, "text %  %  % ") % 12 % 98.876 % 55; 
-    format(file,    "text 125 56 \n 98.76 66,55\n");
+    //format(bt, "text %  %  % ") % 12 % 98.876 % 55; 
+    format(bt,    "text 125 56 \n 98.76 66,55\n");
     display.format( "text 123 56 \n 98.76 66,55\n");
     
-    file.rewind(); // set position indicator to start => read from start
+    bt.rewind(); // set position indicator to start => read from start
 
     char text[5] = {'\0'};
-    text[0] = file.readChar();
-    text[1] = file.readChar();
-    text[2] = file.readChar();
-    text[3] = file.readChar();
+    text[0] = bt.readChar();
+    text[1] = bt.readChar();
+    text[2] = bt.readChar();
+    text[3] = bt.readChar();
     display.format("text: % \n") % text;
 
     uint8_t ui8 = 0; 
-    int er = file.readNumber(ui8);
+    int er = bt.readNumber(ui8);
     display.format("rN(ui8)=% : % \n") % er % ui8;
 
     float f = -1; 
-    er = file.readNumber(f);
+    er = bt.readNumber(f);
     display.format("rN(f)=% : % \n") % er % f;
 
     double d = -1; 
-    er = file.readNumber(d);
+    er = bt.readNumber(d);
     display.format("rN(f)=% : % \n") % er % d;
 
     f = -1; 
-    er = file.readNumber(f);
+    er = bt.readNumber(f);
     display.format("rN(f)=% : % \n") % er % f;
 
     tslp_tsk(2000);
 
     int i = -1; 
-    er = file.readNumber(i);
+    er = bt.readNumber(i);
     display.format("rN(i)=% : % \n") % er % i;
 
     tslp_tsk(2000);
 
     i = -1; 
-    er = file.readNumber(i);
+    er = bt.readNumber(i);
     display.format("rN(i)=% : % \n") % er % i;
 }
