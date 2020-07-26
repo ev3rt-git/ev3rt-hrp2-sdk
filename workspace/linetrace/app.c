360beead3d6241fcc2038643d59b4ffd99e17ca0
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-07-25 21:09:15 -0400

    lkjh

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -11,7 +11,7 @@
 #define _debug(x)
 #endif
 
-const int color_sensor2 = EV3_PORT_2, color_sensor3 = EV3_PORT_3, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C;
+const int color_sensor2 = EV3_PORT_2, color_sensor3 = EV3_PORT_3, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C, a_motor = EV3_PORT_A;
 
 static void button_clicked_handler(intptr_t button) {
     switch(button) {
@@ -34,21 +34,28 @@ void main_task(intptr_t unused) {
     // Configure motors
     ev3_motor_config(left_motor, LARGE_MOTOR);
     ev3_motor_config(right_motor, LARGE_MOTOR);
+    ev3_motor_config(a_motor, MEDIUM_MOTOR);
 
     // Configure sensors
     ev3_sensor_config(color_sensor2, COLOR_SENSOR);
     ev3_sensor_config(color_sensor3, COLOR_SENSOR);
 
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+    float distance = 0;
     /**
      * PID controller
      */
     float lasterror = 0, integral = 0;
-    while (1) {
-        float error = ev3_color_sensor_get_reflect(color_sensor3) - ev3_color_sensor_get_reflect(color_sensor2);
+    while (distance < 3000) {
+        distance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
+        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
         integral = error + integral * 0.5;
-        float steer = 0.07 * error + 0.5 * integral + 1 * (error - lasterror);
+        float steer = 0.07 * error + 0.4 * integral + 1 * (error - lasterror);
         ev3_motor_steer(left_motor, right_motor, 15, steer);
         lasterror = error;
         tslp_tsk(1);
     }
+    ev3_motor_steer(left_motor, right_motor, 0, 0);
+    ev3_motor_rotate(a_motor,200,10,true);
 }
