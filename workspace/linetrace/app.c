ca08ecc36b7634e7c7ccfd0f1a0f02d263514e77
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-07-25 20:41:18 -0400

    Update app.c

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -10,6 +10,7 @@
 
 #include "ev3api.h"
 #include "app.h"
+#include "stdlib.h"
 
 #define DEBUG
 
@@ -19,28 +20,24 @@
 #define _debug(x)
 #endif
 
-/**
- * Define the connection ports of the sensors and motors.
- * By default, this application uses the following ports:
- * Touch sensor: Port 2
- * Color sensor: Port 3
- * Left motor:   Port B
- * Right motor:  Port C
- */
-const int touch_sensor = EV3_PORT_2, color_sensor = EV3_PORT_3, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C;
+const int color_sensor2 = EV3_PORT_2, color_sensor3 = EV3_PORT_3, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C;
 
 static void button_clicked_handler(intptr_t button) {
     switch(button) {
     case BACK_BUTTON:
 #if !defined(BUILD_MODULE)
         syslog(LOG_NOTICE, "Back button clicked.");
+        ev3_motor_steer(left_motor, right_motor, 0, 0);
+        exit(0);
 #endif
+        ev3_motor_steer(left_motor, right_motor, 0, 0);
+        exit(0);
         break;
     }
 }
 
 void main_task(intptr_t unused) {
-    // Register button hadSSSSSSSSSSSSSSSSSSSSSndlers
+    // Register button handlers
     ev3_button_set_on_clicked(BACK_BUTTON, button_clicked_handler, BACK_BUTTON);
 
     // Configure motors
@@ -48,46 +45,18 @@ void main_task(intptr_t unused) {
     ev3_motor_config(right_motor, LARGE_MOTOR);
 
     // Configure sensors
-    ev3_sensor_config(touch_sensor, TOUCH_SENSOR);
-    ev3_sensor_config(color_sensor, COLOR_SENSOR);
-
-    /**
-     * Calibrate for light intensity of WHITE
-     */
-    ev3_speaker_play_tone(NOTE_C4, 100);
-    tslp_tsk(100);
-    ev3_speaker_play_tone(NOTE_C4, 100);
-    // TODO: Calibrate using maximum mode => 100
-    printf("Press the touch sensor to measure light intensity of WHITE.\n");
-    while(!ev3_touch_sensor_is_pressed(touch_sensor));
-    while(ev3_touch_sensor_is_pressed(touch_sensor));
-    int white = ev3_color_sensor_get_reflect(color_sensor);
-    printf("WHITE light intensity: %d.\n", white);
-
-    /**
-     * Calibrate for light intensity of BLACK
-     */
-    ev3_speaker_play_tone(NOTE_C4, 100);
-    tslp_tsk(100);
-    ev3_speaker_play_tone(NOTE_C4, 100);
-    // TODO: Calibrate using maximum mode => 100
-    // TODO: Calibrate using minimum mode => 0
-    printf("Press the touch sensor to measure light intensity of BLACK.\n");
-    while(!ev3_touch_sensor_is_pressed(touch_sensor));
-    while(ev3_touch_sensor_is_pressed(touch_sensor));
-    int black = ev3_color_sensor_get_reflect(color_sensor);
-    printf("BLACK light intensity: %d.\n", black);
+    ev3_sensor_config(color_sensor2, COLOR_SENSOR);
+    ev3_sensor_config(color_sensor3, COLOR_SENSOR);
 
     /**
      * PID controller
      */
     float lasterror = 0, integral = 0;
-    float midpoint = (white - black) / 2 + black;
     while (1) {
-        float error = midpoint - ev3_color_sensor_get_reflect(color_sensor);
+        float error = ev3_color_sensor_get_reflect(color_sensor3) - ev3_color_sensor_get_reflect(color_sensor2);
         integral = error + integral * 0.5;
-        float steer = 0.07 * error + 0.3 * integral + 1 * (error - lasterror);
-        ev3_motor_steer(left_motor, right_motor, 10, steer);
+        float steer = 0.07 * error + 0.5 * integral + 1 * (error - lasterror);
+        ev3_motor_steer(left_motor, right_motor, 15, steer);
         lasterror = error;
         tslp_tsk(1);
     }
