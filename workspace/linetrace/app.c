cb1deedc0799fa92861f56eb7a2857558cec8efc
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-07-26 20:10:46 -0400

    Update app.c

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -27,6 +27,53 @@ static void button_clicked_handler(intptr_t button) {
     }
 }
 
+static void dashPID(int distance){
+    float lasterror = 0, integral = 0;
+    while (wheelDistance < 1800) {
+        if(ev3_motor_get_counts(a_motor) > 490){
+            ev3_motor_reset_counts(a_motor);
+            //ev3_motor_rotate(a_motor,-500,13,false);
+            
+        }
+        if(ev3_motor_get_counts(a_motor) < -490){
+            ev3_motor_reset_counts(a_motor);
+            //ev3_motor_rotate(a_motor,500,13,false);
+        }
+        float wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
+        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
+        integral = error + integral * 0.5;
+        float steer = 0.05 * error + 0.5 * integral + 0.25 * (error - lasterror);
+        ev3_motor_steer(left_motor, right_motor, 30, steer);
+        lasterror = error;
+        tslp_tsk(1);
+    }
+}
+static void linePID(int distance){
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+    ev3_motor_reset_counts(a_motor);
+    float lasterror = 0, integral = 0;
+    while (wheelDistance < 1800) {
+        if(ev3_motor_get_counts(a_motor) > 490){
+            ev3_motor_reset_counts(a_motor);
+            //ev3_motor_rotate(a_motor,-500,13,false);
+            
+        }
+        if(ev3_motor_get_counts(a_motor) < -490){
+            ev3_motor_reset_counts(a_motor);
+            //ev3_motor_rotate(a_motor,500,13,false);
+        }
+        float wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
+        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
+        integral = error + integral * 0.5;
+        float steer = 0.04 * error + 0.5 * integral + 0.25 * (error - lasterror);
+        ev3_motor_steer(left_motor, right_motor, 30, steer);
+        lasterror = error;
+        tslp_tsk(1);
+    }
+    ev3_motor_steer(left_motor, right_motor, 0, 0);
+}
+
 void main_task(intptr_t unused) {
     // Register button handlers
     ev3_button_set_on_clicked(BACK_BUTTON, button_clicked_handler, BACK_BUTTON);
@@ -43,30 +90,6 @@ void main_task(intptr_t unused) {
     ev3_motor_reset_counts(left_motor);
     ev3_motor_reset_counts(right_motor);
     ev3_motor_reset_counts(a_motor);
-    float distance = 0;
-    /**
-     * PID controller
-     */
-    float lasterror = 0, integral = 0;
-    ev3_motor_rotate(a_motor,500,13,true);
-    while (distance < 3000) {
-        if(ev3_motor_get_counts(a_motor) > 490){
-            ev3_motor_reset_counts(a_motor);
-            ev3_motor_rotate(a_motor,-500,13,false);
-            
-        }
-        if(ev3_motor_get_counts(a_motor) < -490){
-            ev3_motor_reset_counts(a_motor);
-            ev3_motor_rotate(a_motor,500,13,false);
-            
-        }
-        distance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
-        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
-        integral = error + integral * 0.5;
-        float steer = 0.075 * error + 0.5 * integral + 0.4 * (error - lasterror);
-        ev3_motor_steer(left_motor, right_motor, 15, steer);
-        lasterror = error;
-        tslp_tsk(1);
-    }
+    dashPID(1900);
     ev3_motor_steer(left_motor, right_motor, 0, 0);
 }
