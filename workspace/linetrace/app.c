25346d97144b7d84075278468689e5a3cf807a46
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-07-26 20:37:48 -0400

    Update app.c

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -94,7 +94,7 @@ void main_task(intptr_t unused) {
     dashPID(2000);
     ev3_motor_steer(left_motor, right_motor, 20, -100);
     tslp_tsk(1000);
-    dashPID(2000);
+    dashPID(1000);
     ev3_motor_steer(left_motor, right_motor, 0, 0);
 
 }
