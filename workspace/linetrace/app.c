5675dd6962737bc22ef20e1b76e69c30162f2358
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-08-02 20:11:55 -0400

    Update app.c

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -204,7 +204,7 @@ void main_task(intptr_t unused) {
         }
         if(color2 == 3){
             //collect snow from yellow
-            //collect snow from 
+            //collect snow from g
         }
         if(color2 == 4){
             //collect snow from yellow
