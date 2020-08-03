c9e5a731cf1e6bb60ebd973a9a3e9fc2cd0ac40d
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-08-02 20:22:54 -0400

    Update app.c

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -234,5 +234,5 @@ void main_task(intptr_t unused) {
             
         }
     }
-    int ht_nxt_color_sensor_measure_color(color_sensor4)
+    int cht_nxt_color_sensor_measure_color(color_sensor4)
 }
