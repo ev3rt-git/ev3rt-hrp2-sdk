abcc2f6f0978f155cfee5f45e57a9f4a5662faf6
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-03 22:20:24 -0400

    Update app.c

--- workspace/test_file/app.c
+++ workspace/test_file/app.c
@@ -1,13 +1,14 @@
 /**
  * This sample program shows a PID controller for line following.
  *
- * Robot construction: Educator Vehicle
+ * Robot conlcdstruction: Educator Vehicle
  *
  * References:
  * http://robotsquare.com/wp-content/uploads/2013/10/45544_educator.pdf
  * http://thetechnicgear.com/2014/03/howto-create-line-following-robot-using-mindstorms/
  */
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "ev3api.h"
 #include "app.h"
 
@@ -30,13 +31,22 @@
 //const int touch_sensor = EV3_PORT_2, color_sensor = EV3_PORT_3, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C;
 const int left_motor = EV3_PORT_B, right_motor = EV3_PORT_C, color_sensor4=EV3_PORT_4;
 rgb_raw_t *rgb4;
-position = { -1, -1, -1, 0, 0};
+position pos = {-1, -1, -1, 0, 0};
 
 static void button_clicked_handler(intptr_t button) {
     switch(button) {
     case BACK_BUTTON:
         ev3_motor_steer(left_motor, right_motor, 0, 0);
         exit(0);
+
+    }
+}
+
+static void exit_program_with_exception() {
+    switch(button) {
+    case BACK_BUTTON:
+        ev3_motor_steer(left_motor, right_motor, 0, 0);
+        exit(1);
 
     }
 }
@@ -52,11 +62,48 @@ void main_task(intptr_t unused) {
     // Configure sensors
     ev3_sensor_config(color_sensor4, HT_NXT_COLOR_SENSOR);
 
-    bool_t val = ht_nxt_color_sensor_measure_rgb(color_sensor4, rgb4);
+    ht_nxt_color_sensor_measure_rgb(color_sensor4, rgb4);
 
-    int bit1 = 0;
-    int bit2 = 0;
+    int bit1 = -1;
+    int bit2 = -1;
 
+    //decode instructions
+    ev3_motor_steer(left_motor, right_motor, 20, 0);
+    while (rgb4->r < 127) {}
+    int index = 0;
+    for (index < 4) {
+        //read instruction
+        ev3_motor_reset_counts(EV3_PORT_B);
+        ev3_motor_reset_counts(EV3_PORT_C);
+        while (((ev3_motor_get_counts(EV3_PORT_B) + ev3_motor_get_counts(EV3_PORT_C)) / 2) < 20) {}
+        if (((rgb4->r + rgb4->g + rgb4->b) / 3) > 127) {
+            bit1 = 1;
+        } else {
+            bit1 = 0;
+        }
+        ev3_motor_reset_counts(EV3_PORT_B);
+        ev3_motor_reset_counts(EV3_PORT_C);
+        while (((ev3_motor_get_counts(EV3_PORT_B) + ev3_motor_get_counts(EV3_PORT_C)) / 2) < 20) {}
+        if (((rgb4->r + rgb4->g + rgb4->b) / 3) > 127) {
+            bit2 = 1;
+        } else {
+            bit2 = 0;
+        }
+        // decode instruction
+        if (bit1 == 1) {
+            if (bit2 == 1) {
+                exit_program_with_exception();
+            } else {
+                tasks[index] = BLACKMATERIAL;
+            }
+        } else {
+            if (bit2 == 1) {
+                tasks[index] = BLUEMATERIAL;
+            } else {
+                tasks[index] = REMOVESNOW;
+            }
+        }
+    }
     // read instructions for blue road
     ev3_motor_reset_counts(EV3_PORT_B);
     ev3_motor_reset_counts(EV3_PORT_C);
@@ -82,19 +129,18 @@ void main_task(intptr_t unused) {
     if (bit1) {
         if (bit2) {
             exit(1);
-            
         } else {
-            tasks[0] = 2;
+            tasks[BLUE_STREET] = BLACKMATERIAL;
         }
     } else {
         if (bit2) {
-            tasks[0] = 1;
+            tasks[BLUE_STREET] = BLUEMATERIAL;
         } else {
-            tasks[0] = 0;
+            tasks[BLUE_STREET] = REMOVESNOW;
         }
     }
     
-    // read instructions for green road
+ /*   // read instructions for green road
     ev3_motor_reset_counts(EV3_PORT_B);
     ev3_motor_reset_counts(EV3_PORT_C);
     while (rgb4->r < 127) {}
@@ -119,17 +165,17 @@ void main_task(intptr_t unused) {
         if (bit2) {
             exit(1);
         } else {
-            tasks[1] = 2;
+            tasks[GREEN_STREET] = BLACKMATERIAL;
         }
     } else {
         if (bit2) {
-            tasks[1] = 1;
+            tasks[GREEN_STREET] = BLUEMATERIAL;
         } else {
-            tasks[1] = 0;
+            tasks[GREEN_STREET] = REMOVESNOW;
         }
     }
     
-    // read instructions for yellow road
+    // read inlcdstructions for yellow road
     ev3_motor_reset_counts(EV3_PORT_B);
     ev3_motor_reset_counts(EV3_PORT_C);
     while (rgb4->r < 127) {}
@@ -154,17 +200,17 @@ void main_task(intptr_t unused) {
         if (bit2) {
             exit(1);
         } else {
-            tasks[2] = 2;
+            tasks[YELLOW_STREET] = BLACKMATERIAL;
         }
     } else {
         if (bit2) {
-            tasks[2] = 1;
+            tasks[YELLOW_STREET] = BLUEMATERIAL;
         } else {
-            tasks[2] = 0;
+            tasks[YELLOW_STREET] = REMOVESNOW;
         }
     }
     
-    // read instructions for red road
+    // read inlcdstructions for red road
     ev3_motor_reset_counts(EV3_PORT_B);
     ev3_motor_reset_counts(EV3_PORT_C);
     while (rgb4->r < 127) {}
@@ -189,25 +235,22 @@ void main_task(intptr_t unused) {
         if (bit2) {
             exit(1);
         } else {
-            tasks[3] = 2;
+            tasks[RED_STREET] = BLACKMATERIAL;
         }
     } else {
         if (bit2) {
-            tasks[3] = 1;
+            tasks[RED_STREET] = BLUEMATERIAL;
         } else {
-            tasks[3] = 0;
+            tasks[RED_STREET] = REMOVESNOW;
         }
     }
 
-    //display things in a very small font
-    char val1 = tasks[0];
-    char val2 = tasks[1];
-    char val3 = tasks[2];
-    char val4 = tasks[3];
+ */
+   //display things in a very small font
+    char lcdstr = char[100];
+    sprintf(lcdstr, "%d, %d, %d, %d", tasks[BLUE_STREET], tasks[RED_STREET], tasks[GREEN_STREET], tasks[YELLOW_STREET]);
     ev3_lcd_set_font(EV3_FONT_MEDIUM);
-    ev3_lcd_draw_string(val1, 5, 0);
-    ev3_lcd_draw_string(val2, 5, 20);
-    ev3_lcd_draw_string(val3, 5, 40);
-    ev3_lcd_draw_string(val4, 5, 60);
+    ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, COLOR_WHITE);
+    ev3_lcd_draw_lcdstring(lcdstr, 0, 0);
     while (1>0) {}
 }
