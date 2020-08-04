b417278cdf4ae4f20e9150203894df429bfbbd27
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-03 22:20:28 -0400

    Update app.h

--- workspace/test_file/app.h
+++ workspace/test_file/app.h
@@ -6,14 +6,14 @@
 #ifndef _TEST_FILE_H_
 #define _TEST_FILE_H_
 
-#define BLUE_STREET 0;
-#define GREEN_STREET 1;
-#define YELLOW_STREET 2;
-#define RED_STREET 3;
+#define BLUE_STREET 0
+#define GREEN_STREET 1
+#define YELLOW_STREET 2
+#define RED_STREET 3
 
-#define REMOVESNOW 0;
-#define BLUEMATERIAL 1;
-#define BLACKMATERIAL 2;
+#define REMOVESNOW 0
+#define BLUEMATERIAL 1
+#define BLACKMATERIAL 2
 
 typedef struct {
 	int street;
@@ -23,7 +23,7 @@ typedef struct {
 	int facing;
 } position;
 
-static tasks[4] = {0, 0, 0, 0};
+static int tasks[4] = {0, 0, 0, 0};
 
 extern void	main_task(intptr_t exinf);
 #endif /* TOPPERS_MACRO_ONLY */
