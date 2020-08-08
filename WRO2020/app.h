206ad9b7e149bf5f22b7dbbee41dc4cdaf21accc
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-08 19:35:29 -0400

    Added config() and fixed displayValues(), updated header
    Description

--- WRO2020/app.h
+++ WRO2020/app.h
@@ -21,8 +21,9 @@
 void linePID();
 void dashPID();
 void readCode();
-static void button_clicked_handler();
+void config();
 void display_values();
+static void button_clicked_handler();
 
 
 #define BLUE_STREET 	0
@@ -34,6 +35,24 @@ void display_values();
 #define BLUEMATERIAL 	1
 #define BLACKMATERIAL 	2
 
+/*
+* street:
+* RED, YELLOW, GREEN, or BLUE
+* 
+* section:
+* RED and YELLOW: Section 1 is the area containing the instructions
+* GREEN and BLUE: Section 1 is the area where the road goes diagonaly into their respective streets
+* Section 1 is the "beginning" of the road, and 3 being the "end"
+* 
+* distance:
+* Distance in centimeters from the start of the road
+* 
+* dash:
+* (Only applicable for streets RED and YELLOW) Dashes from the start of the section, with a dash being a black line
+* 
+* facing:
+* direction in degrees of where the robot is facing, 0 being the same as when it started, and ranges from -180 to 179.
+*/
 typedef struct {
 	int street;
 	int section;
