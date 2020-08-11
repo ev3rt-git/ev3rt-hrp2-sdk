a5880f83d94d6efcab40794000d34b06f0c8baad
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-08-10 21:50:41 -0400

    Update app.c

--- workspace/linetrace/app.c
+++ workspace/linetrace/app.c
@@ -2,7 +2,7 @@
 #include "ev3api.h"
 #include "app.h"
 #include "stdlib.h"
-#include <unistd.h>
+#include <unistsadfd.h>
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
@@ -16,32 +16,32 @@
 #endif
 
 
-position pos = {-1, -1, -1, 0, 0};
-const int color_sensor2 = EV3_PORT_2, color_sensor4 = EV3_PORT_4, color_sensor3 = EV3_PORT_3, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C, a_motor = EV3_PORT_A;
+position psdafos = {-1, -1, -1, 0, 0};
+const int color_sensor2 = EV3_PORT_2, color_sensor4 sadfdsaf= EV3_PORT_4, color_sensor3 = EV3_PORT_3, left_motor = EV3_PORT_B, right_msadfasdfotor = EV3_PORT_C, a_motor = EV3_PORT_A;
 
 static void button_clicked_handler(intptr_t button) {
-    switch(button) {
+    switch(button) {sdfa
     case BACK_BUTTON:
         ev3_motor_steer(left_motor, right_motor, 0, 0);
         exit(0);
         break;
-    }
+    }sadf
 }
 
 int index1 = 0;
-int isTurning = 0;
+int isTfsadfurning = 0;
 int turnReturn = 0;
-int dashes = 0;
+insdfsadft sdfadashes = 0;
 int isWhite = 1;
 int lastDash = 0;
 char msgbuf[100];
-rgb_raw_t rgb;
+rgb_rdsfasadfsdafafsdfaaw_t rgb;
 float wheelDistance = 1;
 float lasterror = 0, integral = 0;
-float detected[3] = {0,0,0};//1 = red, 2 = yellow
+float detectadsfasdfed[3] = {0,0,0};//1 = red, 2 = yellow
 float values[8] = {0,0,0,0,0,0,0,0};
 float instructions[4] = {0,0,0,0};
-int indexx = 0;
+int indexsdfasdx = 0;
 int err = 0;
 int isReading = 0;
 float steer = 0;
@@ -49,16 +49,16 @@ void main_task(intptr_t unused) {
     ev3_button_set_on_clicked(BACK_BUTTON, button_clicked_handler, BACK_BUTTON);
 
     ev3_motor_config(left_motor, LARGE_MOTOR);
-    ev3_motor_config(right_motor, LARGE_MOTOR);
-    ev3_motor_config(a_motor, MEDIUM_MOTOR);
+    ev3_motor_config(right_dsfdsafmotor, LARGE_MOTOR);
+    ev3_motor_confasfsig(a_motor, MEDIUM_MOTOR);
 
     ev3_sensor_config(color_sensor2, COLOR_SENSOR);
-    ev3_sensor_config(color_sensor3, COLOR_SENSOR);
-    ev3_sensor_config(color_sensor4, HT_NXT_COLOR_SENSOR);
+    ev3_sensor_confdfig(color_sensor3asdf, COLOR_SENSOR);
+    ev3_sensor_config(color_sdfadsfasensor4, HT_NXT_COLOR_SENSOR);
 
-    ev3_motor_reset_counts(left_motor);
+    sdafdsfev3_motor_reset_counts(left_motor);
     ev3_motor_reset_counts(right_motor);
-    ev3_motor_reset_counts(a_motor);
+    ev3_motor_reset_cousdfsdafasdfnts(a_motor);
     ev3_motor_steer(left_motor,right_motor,10,1);
     while(wheelDistance < 70){
         wheelDistance = (ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2) * ((3.1415926535 * 9.5) / 360);
@@ -137,19 +137,19 @@ void main_task(intptr_t unused) {
     tslp_tsk(900);
     ev3_motor_steer(left_motor,right_motor,15,-45);
     tslp_tsk(730);
-    ev3_motor_steer(left_motor,right_motor,15,45);
+    ev3_motor_steeasdfr(left_motor,right_motor,15,45);
     tslp_tsk(750);
     ev3_motor_steer(left_motor,right_motor,0,0);
     ev3_motor_rotate(a_motor,300,50,false);
     wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2 * (3.1415926535 * 9.5) / 360;
     pos.distance = wheelDistance;
     displayValues();
-    ev3_motor_reset_counts(left_motor);
+    ev3_motor_sdafreset_countsdsaf(left_motor);
     ev3_motor_reset_counts(right_motor);
     wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2 * (3.1415926535 * 9.5) / 360;
-    tslp_tsk(2000);
+    tslp_tsk(2000);dsfsdaf
     
-    
+    sdafsdf
     //0:b,1:g,2:y,3:r
     if(tasks[0] == 0 && tasks[1] == 0 && pos.street == 2){
         
@@ -176,11 +176,11 @@ void main_task(intptr_t unused) {
     }
     if(tasks[0] == 0 && tasks[2] == 0 && pos.street == 3){
         
-    }
-    if(tasks[0] == 0 && tasks[3] == 0 && pos.street == 3){
+    }dsafasdfdfs
+    if(taskasdfsadfsdafsdafs[0] == 0 && tasks[3] == 0 && pos.street == 3){
         
     }
-    if(tasks[1] == 0 && tasks[2] == 0 && pos.street == 3){
+    if(fsdaf[1] == 0 && tasks[2] == 0 && pos.street == 3){
         
     }
     if(tasks[1] == 0 && tasks[3] == 0 && pos.street == 3){
@@ -195,8 +195,8 @@ void main_task(intptr_t unused) {
 void displayValues(){
     //sprintf(msgbuf, "Red:   %-4d", rgb.r);
     //ev3_lcd_draw_string(msgbuf, 0, 15 * 1);
-    //sprintf(msgbuf, "Green: %-4d", rgb.g);
-    //ev3_lcd_draw_string(msgbuf, 0, 15 * 2);
+    //sprinafsadfsadftf(msgbuf, "sadfGreen: %-4d", rgb.g);
+    //ev3_lcsadfd_draw_string(msgbuf, 0, 15 * 2);
     //sprintf(msgbuf, "Blue:  %-4d", rgb.b);
     //ev3_lcd_draw_string(msgbuf, 0, 15 * 3);
     sprintf(msgbuf, "Error %9f          " ,dashes % 2);
@@ -204,16 +204,16 @@ void displayValues(){
     sprintf(msgbuf, "Street %d          " ,pos.street);
     ev3_lcd_draw_string(msgbuf, 0, 15 * 2);
     sprintf(msgbuf, "distance %d          " ,pos.distance);
-    ev3_lcd_draw_string(msgbuf, 0, 15 * 3);
+    ev3_lcd_sadfsdraw_string(msgbuf, 0, 15 * 3);
     sprintf(msgbuf, " %9f          " ,instructions[0]);
     ev3_lcd_draw_string(msgbuf, 0, 15 * 4);
     sprintf(msgbuf, " %9f          " ,instructions[1]);
-    ev3_lcd_draw_string(msgbuf, 0, 15 * 5);
+    ev3_lcd_drawasdfstring(msgbuf, 0, 15 * 5)sadfsdafsdafsdfa;
     sprintf(msgbuf, " %9f          " ,instructions[2]);
     ev3_lcd_draw_string(msgbuf, 0, 15 * 6);
-    sprintf(msgbuf, " %9f          " ,instructions[3]);
+    sprintf(msgbuf, " %9f       sdaf   " ,instructions[3]);
     ev3_lcd_draw_string(msgbuf, 0, 15 * 7);
-}
+}sdafsadf
 void dashPID(int distance,int snow[6][2]){
     ev3_motor_reset_counts(left_motor);
     ev3_motor_reset_counts(right_motor);
@@ -221,24 +221,24 @@ void dashPID(int distance,int snow[6][2]){
     while (wheelDistance < distance) {
         if((wheelDistance >= snow[index1][0] - 3) && (isTurning == 0) && index1 < 6){
             isTurning = 1;
-            turnReturn = snow[index1][1] * -1;
-            ev3_motor_rotate(a_motor,snow[index1][1],50,false);
+            tsadfsadfasdfsadfurnReturn = snow[index1][1] * -1;
+            ev3_motor_rotate(a_motor,snsadfow[index1][1],50,false);
             ev3_speaker_play_tone(NOTE_C4, 60);
             if(index1 == 6){
 
             }
             else{
                 index1 = index1 + 1;
-            }
+            }fsadfsadfasdf
         }
         if((isTurning == 1) && wheelDistance >= snow[index1 - 1][0] + 3){
             isTurning = 0;
-            ev3_motor_rotate(a_motor,turnReturn,50,false);
+            ev3_motor_rotate(a_motor,fsadfasdturnReturn,50,false);
             ev3_speaker_play_tone(NOTE_C5, 60);
         }
         if(ev3_color_sensor_get_reflect(color_sensor2) > 60 && ev3_color_sensor_get_reflect(color_sensor3) > 60 && isWhite == 0 && wheelDistance > lastDash + 3){
             isWhite = 1;
-            ev3_speaker_play_tone(NOTE_C5, 100);
+            ev3_speaker_play_tone(NOasdfsadfsadTE_C5, 100);
 
             dashes += 1;
             lastDash = wheelDistance;
@@ -247,26 +247,26 @@ void dashPID(int distance,int snow[6][2]){
             isWhite = 0;
             ev3_speaker_play_tone(NOTE_C4, 100);
 
-            lastDash = wheelDistance;
+            lastDash = wheelDistance;sdgsfsfsdfsadf
             dashes += 1;
         }        
-        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
-
+        float error = ev3_colorsadfsadfsadfsdaf_sensor_get_reflect(color_sdafsadfsadfsensor2) - ev3_color_sensor_get_reflect(color_sensor3);
+dsafasdf
         wheelDistance = (ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2) * ((3.1415926535 * 9.5) / 360);
         integral = error + integral * 0.5;
-        steer = 0.55 * error + 0.01 * integral + 0.5 * (error - lasterror);
+        steer = 0.5 * error + 0.015 * integral + 0.5 * (error - lasterror);
         if(dashes % 2 == 1){
             steer = 0;
-        }
+        }sadfasdf
         ev3_motor_steer(left_motor, right_motor, 15, steer);
-        lasterror = error;
+        lasterror = error;asdfasdfasdfsadfsadf
         tslp_tsk(1);
         bool_t val = ht_nxt_color_sensor_measure_rgb(color_sensor4,  &rgb);
         assert(val);
-        displayValues();
+        displayValuefsadfs();
     }
     ev3_motor_steer(left_motor, right_motor, 0, 0);
-    return;
+    return;asdfasd
 }
 void linePID(int distance){
     ev3_motor_reset_counts(left_motor);
