bcb116eadd2e63d3d7c299cf1e53fd356bd45bf8
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-15 19:59:01 -0400

    Update app.c

--- workspace/test_file/app.c
+++ workspace/test_file/app.c
@@ -41,35 +41,7 @@ void main_task(intptr_t unused) {
     tslp_tsk(50000000);
 }
 
-void linePID(int distance){
-    ev3_motor_reset_counts(left_motor);
-    ev3_motor_reset_counts(right_motor);
-    ev3_motor_reset_counts(a_motor);
-    ev3_motor_reset_counts(d_motor);
-    ev3_motor_rotate(a_motor,-500,15,true);
-    float wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
-    float lasterror = 0, integral = 0;
-    while (wheelDistance < distance) {
-        if(ev3_motor_get_counts(a_motor) > 490){
-            ev3_motor_reset_counts(a_motor);
-            ev3_motor_rotate(a_motor,-500,13,false);
-            
-        }
-        if(ev3_motor_get_counts(a_motor) < -490){
-            ev3_motor_reset_counts(a_motor);
-            ev3_motor_rotate(a_motor,500,13,false);
-        }
-        wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
-        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
-        integral = error + integral * 0.5;
-        float steer = 0.04 * error + 0.5 * integral + 0.25 * (error - lasterror);
-        ev3_motor_steer(left_motor, right_motor, 30, steer);
-        lasterror = error;  
-        tslp_tsk(1);
-    }
-    ev3_motor_steer(left_motor, right_motor, 0, 0);
-    return;
-}
+
 
 void readCode() {
     // define variables
