82f25f2dd5243dcbb33ad64d0ffc9dcac7229560
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-08-15 21:55:03 -0400

    Merge Linetrace  to WRO2020

--- workspace/WRO2020/app.c
+++ workspace/WRO2020/app.c
@@ -110,7 +110,7 @@ void readCode() {
     ev3_motor_steer(left_motor, right_motor, 0, 0);
     tslp_tsk(5);
 
-/*
+    /*
     // align robot
     ev3_motor_rotate(left_motor, -100, 15, true);
     ev3_motor_reset_counts(left_motor);
@@ -127,7 +127,7 @@ void readCode() {
         display_values();
     }
     ev3_motor_steer(left_motor, right_motor, 0, 0);
-*/
+    */
 
     // display things in a very medium font
     ev3_lcd_fill_rect(0, 0, 178, 128, EV3_LCD_WHITE);
@@ -211,7 +211,7 @@ void display_values() {
     ev3_lcd_draw_string(msg, 10*8, 15*0);
 
     // read sensor rgb1
-/*
+    /*
     bool_t val1 = ht_nxt_color_sensor_measure_rgb(color_sensor1, &rgb1);
     assert(val1);
     sprintf(msg, "RGB1:");
@@ -222,7 +222,7 @@ void display_values() {
     ev3_lcd_draw_string(msg, 10*7, 15*2.5);
     sprintf(msg, "B: %d", rgb1.b);
     ev3_lcd_draw_string(msg, 10*14, 15*2.5);
-*/
+    */
 
     // read sensor rgb4
     bool_t val4 = ht_nxt_color_sensor_measure_rgb(color_sensor4, &rgb4);
@@ -245,6 +245,247 @@ void display_values() {
     value = ev3_color_sensor_get_reflect(color_sensor3);
     sprintf(msg, "L: %d  ", value);
     ev3_lcd_draw_string(msg, 10*7, 15*7.5);
+}
+
+void readColorCode(){
+    float wheelDistance = 0;
+    float detected[3] = {0,0,0};//1 = red, 2 = yellow
+    float values[8] = {0,0,0,0,0,0,0,0};
+    int instructions[4] = {0,0,0,0};
+    int readIndex = 0;
+    int isReading = 0;
+    ev3_motor_steer(left_motor,right_motor,10,5);
+    while(wheelDistance < 70){
+        wheelDistance = (ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2) * ((3.1415926535 * 9.5) / 360);
+        bool_t val = ht_nxt_color_sensor_measure_rgb(color_sensor4,  &rgb4);
+        assert(val);
+        if(readIndex == 0 && rgb4.g > 40 && rgb4.r > 40 && wheelDistance > 30 && wheelDistance < 35){
+            readIndex += 1;
+            detected[0] = 2;
+            ev3_speaker_play_tone(NOTE_A6, 60);
+        }
+        else if(readIndex == 0 && rgb4.r > 45 && wheelDistance > 30 && wheelDistance < 35){
+            readIndex += 1;
+            detected[0] = 1;
+            ev3_speaker_play_tone(NOTE_A4, 60);
+        }
+        /*else if(rgb4.r > 55 && isReading < 2 && wheelDistance > 35 && readIndex > 0){
+            isReading = 50;
+            detected[readIndex] = wheelDistance;
+            readIndex += 1;
+            ev3_speaker_play_tone(NOTE_C5, 60);
+        }
+        else if(rgb4.g > 55 && isReading < 2 && wheelDistance > 35 && readIndex > 0){
+            isReading = 50;
+            detected[readIndex] = wheelDistance;
+            readIndex += 1;
+            ev3_speaker_play_tone(NOTE_C5, 60);
+        }
+        else if(rgb4.b > 55 && isReading < 2 && wheelDistance > 35 && readIndex > 0){
+            isReading = 50;
+            detected[readIndex] = wheelDistance;
+            readIndex += 1;
+            ev3_speaker_play_tone(NOTE_C5, 60);
+        }*/
+        //else if(isReading > 1){
+            //isReading = isReading - 1;
+        //}
+        int sddsfsd = round((detected[1] - 31) / 5);
+        int sddsfsd2 = round((detected[2] - 31) / 5);
+        values[sddsfsd] = 1;
+        values[sddsfsd2] = 1;
+        for(int i = 0; i < 7; i +=2){
+            if(values[i] == 0){
+                if(values[i + 1] == 0){
+                    instructions[i/2] = 0;
+                    tasks[i/2] = 0;
+                }
+                else{
+                    instructions[i/2] = 1;
+                    tasks[i/2] = 1;
+                }
+            }
+            else{
+                if(values[i + 1] == 0){
+                    instructions[i/2] = 2;
+                    tasks[i/2] = 2;
+                }
+                else{
+                }
+            }
+        }
+        if(detected[0] = 2){
+            pos.street = 2;
+        }
+        if(detected[0] = 1){
+            pos.street = 3;
+        }
+        pos.section = 1;
+        pos.distance = wheelDistance;
+        pos.dash = 0;
+        pos.facing = 0;
+        tslp_tsk(10);
+    }
+    ev3_motor_steer(left_motor,right_motor,15,0);
+    tslp_tsk(1500);
+    ev3_motor_steer(left_motor,right_motor,0,0);
+    wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2 * (3.1415926535 * 9.5) / 360;
+    pos.distance = wheelDistance;
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+    wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2 * (3.1415926535 * 9.5) / 360;
+}
+
+void execute_moving_the_robot_based_on_the_color_code(){
+    //0:b,1:g,2:y,3:r
+    if(tasks[0] == 0 && tasks[1] == 0 && pos.street == 2){
+        
+    }
+    if(tasks[0] == 0 && tasks[2] == 0 && pos.street == 2){
+        
+    }
+    if(tasks[0] == 0 && tasks[3] == 0 && pos.street == 2){
+        
+    }
+    if(tasks[1] == 0 && tasks[2] == 0 && pos.street == 2){
+        
+    }
+    if(tasks[1] == 0 && tasks[3] == 0 && pos.street == 2){
+        
+    }
+    if(tasks[2] == 0 && tasks[3] == 0 && pos.street == 2){
+        int snowValues[6][3] = {{17,-300},{121,-300},{139,0},{1000,0},{1000,0},{1000,0}};
+        wallFollow(160,snowValues);
+    }
+    //red
+    if(tasks[0] == 0 && tasks[1] == 0 && pos.street == 3){
+
+    }
+    if(tasks[0] == 0 && tasks[2] == 0 && pos.street == 3){
+        
+    }
+    if(tasks[0] == 0 && tasks[3] == 0 && pos.street == 3){
+        
+    }
+    if(tasks[1] == 0 && tasks[2] == 0 && pos.street == 3){
+        
+    }
+    if(tasks[1] == 0 && tasks[3] == 0 && pos.street == 3){
+        
+    }
+    if(tasks[2] == 0 && tasks[3] == 0 && pos.street == 3){
+        int snowValues[6][3] = {{25,350,5},{70,275,10},{97,350,6},{1000,0,0},{1000,0,0},{1000,0,0}};
+        wallFollow(136,snowValues,3);
+        ev3_motor_rotate(a_motor,500,50,true);
+        ev3_motor_steer(left_motor,right_motor,-15,0);
+        tslp_tsk(1000);
+        ev3_motor_steer(left_motor,right_motor,0,0);
+        tslp_tsk(1050);
+        ev3_motor_steer(left_motor,right_motor,-15,90);
+        tslp_tsk(900);
+        ev3_motor_steer(left_motor,right_motor,0,0);
+        ev3_motor_steer(left_motor,right_motor,-15,0);
+        tslp_tsk(900);
+        ev3_motor_steer(left_motor,right_motor,0,0);
+        ev3_motor_rotate(a_motor,-500,50,true);
+        tslp_tsk(1000);
+        int snowValues1[6][3] = {{23,500,10},{1000,0,0},{1000,0,0},{1000,0,0},{1000,0,0},{1000,0,0}};
+        wallFollow(60,snowValues1,0);
+        ev3_motor_steer(left_motor,right_motor,20,0);
+        tslp_tsk(1600);
+        ev3_motor_steer(left_motor,right_motor,0,0);
+        ev3_motor_steer(left_motor,right_motor,10,-45);
+        tslp_tsk(2400);
+        ev3_motor_steer(left_motor,right_motor,0,0);
+        int snowValues2[6][3] = {{20,500,5},{1000,0,0},{1000,0,0},{1000,0,0},{1000,0,0},{1000,0,0}};
+        wallFollow(160,snowValues2,3);
+    }
+}
+
+void wallFollow(int distance,int snow[6][3],int steer){
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+    ev3_motor_reset_counts(a_motor);
+    ev3_motor_reset_counts(d_motor);
+    int dashes = 0;
+    int isWhite = 1;
+    int lastDash = 0;
+    int snowIndex = 0;
+    int isTurning = 0;
+    float wheelDistance = 0;
+    while (wheelDistance < distance) {
+        if(wheelDistance >= snow[snowIndex][0] - snow[snowIndex][2] && isTurning == 0 && snowIndex < 6){
+            isTurning = 1;
+            ev3_motor_rotate(a_motor,snow[snowIndex][1],50,false);
+            ev3_speaker_play_tone(NOTE_C4, 60);
+            if(snowIndex == 6){
+
+            }
+            else{
+                snowIndex = snowIndex + 1;
+            }
+        }
+        if(isTurning == 1 && wheelDistance >= snow[snowIndex - 1][0] + snow[snowIndex - 1][2]){
+            isTurning = 0;
+            ev3_motor_rotate(a_motor,snow[snowIndex - 1][1] * -1,50,false);
+            ev3_speaker_play_tone(NOTE_C5, 60);
+        }
+        /*if(ev3_color_sensor_get_reflect(color_sensor2) > 60 && isWhite == 0 && wheelDistance > lastDash + 3){
+            isWhite = 1;
+            ev3_speaker_play_tone(NOTE_C5, 100);
+
+            dashes += 1;
+            lastDash = wheelDistance;
+        }
+        if(ev3_color_sensor_get_reflect(color_sensor2) < 60 && isWhite == 1 && wheelDistance > lastDash + 3){
+            isWhite = 0;
+            ev3_speaker_play_tone(NOTE_C4, 100);
+
+            lastDash = wheelDistance;
+            dashes += 1;
+        }*/
+        //float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
+
+        wheelDistance = (ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2) * ((3.1415926535 * 9.5) / 360);
+        ev3_motor_steer(left_motor, right_motor, 15, steer);
+        //lasterror = error;
+        tslp_tsk(1);
+        bool_t val = ht_nxt_color_sensor_measure_rgb(color_sensor4,  &rgb4);
+        assert(val);
+    }
+    ev3_motor_steer(left_motor, right_motor, 0, 0);
+    return;
+}
+
+void linePID(int distance){
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+    ev3_motor_reset_counts(a_motor);
+    ev3_motor_reset_counts(d_motor);
+    int snowIndex = 0;
+    int isTurning = 0;
+    float wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
+    float lasterror = 0, integral = 0;
+    while (wheelDistance < distance) {
+        if(ev3_motor_get_counts(a_motor) > 490){
+            ev3_motor_reset_counts(a_motor);
+            ev3_motor_rotate(a_motor,-500,13,false);
+            
+        }
+        if(ev3_motor_get_counts(a_motor) < -490){
+            ev3_motor_reset_counts(a_motor);
+            ev3_motor_rotate(a_motor,500,13,false);
+        }
+        wheelDistance = ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2;
+        float error = ev3_color_sensor_get_reflect(color_sensor2) - ev3_color_sensor_get_reflect(color_sensor3);
+        integral = error + integral * 0.5;
+        float steer = 0.04 * error + 0.5 * integral + 0.25 * (error - lasterror);
+        ev3_motor_steer(left_motor, right_motor, 30, steer);
+        lasterror = error;  
+        tslp_tsk(1);
+    }
+    ev3_motor_steer(left_motor, right_motor, 0, 0);
+    return;
 }
 
 static void button_clicked_handler(intptr_t button) {
--- workspace/WRO2020/app.h
+++ workspace/WRO2020/app.h
@@ -1,38 +1,20 @@
-
 #include "target_test.h"
-
-#define MAIN_PRIORITY	5
-
-#define HIGH_PRIORITY	9
-#define MID_PRIORITY	10
-#define LOW_PRIORITY	11
-
-#ifndef STACK_SIZE
-#define	STACK_SIZE		4096
-#endif
-#ifndef LOOP_REF
-#define LOOP_REF		ULONG_C(1000000)
-#endif
 
 #ifndef _TEST_FILE_H_
 #define _TEST_FILE_H_
 
-void linePID();
-void wallFollow();
+#define BLUE_STREET 0
+#define GREEN_STREET 1
+#define YELLOW_STREET 2
+#define RED_STREET 3
+
+#define REMOVESNOW 0
+#define BLUEMATERIAL 1
+#define BLACKMATERIAL 2
+
 void readCode();
-void init();
-void display_values();
 static void button_clicked_handler();
-
-
-#define BLUE_STREET 	0
-#define GREEN_STREET 	1
-#define YELLOW_STREET 	2
-#define RED_STREET 		3
-
-#define REMOVESNOW 		0
-#define BLUEMATERIAL 	1
-#define BLACKMATERIAL 	2
+void display_values();
 
 /*
 * street:
@@ -55,39 +37,12 @@ static void button_clicked_handler();
 typedef struct {
 	int street;
 	int section;
-	int distance;
+	float distance;
 	int dash;
 	int facing;
 } position;
 
 static int tasks[4] = {0, 0, 0, 0};
 
-
-
 extern void	main_task(intptr_t exinf);
-
-#ifndef TOPPERS_MACRO_ONLY
-
-extern void	task(intptr_t exinf);
-extern void	main_task(intptr_t exinf);
-extern void balance_task(intptr_t exinf);
-extern void idle_task(intptr_t exinf);
-//extern void	tex_routine(TEXPTN texptn, intptr_t exinf);
-//#ifdef CPUEXC1
-//extern void	cpuexc_handler(void *p_excinf);
-//#endif /* CPUEXC1 */
-//extern void	cyclic_handler(intptr_t exinf);
-//extern void	alarm_handler(intptr_t exinf);
-//
-//extern void	gpio_handler_initialize(intptr_t exinf);
-//extern void	gpio_handler(void);
-extern void	gpio_irq_dispatcher(intptr_t exinf);
-//
-//extern void	uart_sensor_monitor(intptr_t exinf);
-//
-//extern void	ev3_uart_cyclic_handler(intptr_t exinf);
-//extern void	ev3_uart_daemon(intptr_t exinf);
-//extern void	ev3_uart_port2_irq(void);
-//
-//extern void initialize_ev3(intptr_t exinf);
-#endif
+#endif /* TOPPERS_MACRO_ONLY */
